8b6283f6b22b26bfeee1386ff5f3539f9becad2d
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-06-10 10:28:52 +0300

    Created EntityBase class.

---
+++ TransactionReportsStorage/TransactionReportsStorage.Core/Common/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TransactionReportsStorage.Core.Common
+{
+    public abstract class EntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
