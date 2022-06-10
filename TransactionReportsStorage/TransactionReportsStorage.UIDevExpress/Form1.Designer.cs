4902e9ab8c8855f0edbe77e70cf77b3e4df7d03e
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-06-10 14:14:03 +0300

    Added searching with filters.

--- TransactionReportsStorage/TransactionReportsStorage.UIDevExpress/Form1.Designer.cs
+++ TransactionReportsStorage/TransactionReportsStorage.UIDevExpress/Form1.Designer.cs
@@ -33,8 +33,21 @@
             this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
             this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.dateEdit1 = new DevExpress.XtraEditors.DateEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.dateEdit2 = new DevExpress.XtraEditors.DateEdit();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit2.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit2.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // gridControl1
@@ -77,11 +90,89 @@
             this.labelControl1.Size = new System.Drawing.Size(0, 13);
             this.labelControl1.TabIndex = 3;
             // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(77, 21);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(100, 20);
+            this.textEdit1.TabIndex = 4;
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(24, 24);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(47, 13);
+            this.labelControl2.TabIndex = 5;
+            this.labelControl2.Text = "Cell Name";
+            // 
+            // dateEdit1
+            // 
+            this.dateEdit1.EditValue = null;
+            this.dateEdit1.Location = new System.Drawing.Point(77, 59);
+            this.dateEdit1.Name = "dateEdit1";
+            this.dateEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dateEdit1.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dateEdit1.Size = new System.Drawing.Size(100, 20);
+            this.dateEdit1.TabIndex = 6;
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(24, 62);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(28, 13);
+            this.labelControl3.TabIndex = 7;
+            this.labelControl3.Text = "From:";
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Location = new System.Drawing.Point(24, 101);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(16, 13);
+            this.labelControl4.TabIndex = 9;
+            this.labelControl4.Text = "To:";
+            // 
+            // dateEdit2
+            // 
+            this.dateEdit2.EditValue = null;
+            this.dateEdit2.Location = new System.Drawing.Point(77, 98);
+            this.dateEdit2.Name = "dateEdit2";
+            this.dateEdit2.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dateEdit2.Size = new System.Drawing.Size(100, 20);
+            this.dateEdit2.TabIndex = 8;
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.Location = new System.Drawing.Point(63, 151);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton3.TabIndex = 10;
+            this.simpleButton3.Text = "Search";
+            this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click);
+            // 
+            // labelControl5
+            // 
+            this.labelControl5.Location = new System.Drawing.Point(24, 204);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(94, 13);
+            this.labelControl5.TabIndex = 11;
+            this.labelControl5.Text = "*Fill all filters to use";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(819, 425);
+            this.Controls.Add(this.labelControl5);
+            this.Controls.Add(this.simpleButton3);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.dateEdit2);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.dateEdit1);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.textEdit1);
             this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.simpleButton2);
             this.Controls.Add(this.simpleButton1);
@@ -90,6 +181,11 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit2.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit2.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,6 +198,14 @@
         private DevExpress.XtraEditors.SimpleButton simpleButton1;
         private DevExpress.XtraEditors.SimpleButton simpleButton2;
         private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.DateEdit dateEdit1;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.DateEdit dateEdit2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
     }
 }
 
--- TransactionReportsStorage/TransactionReportsStorage.UIDevExpress/Form1.cs
+++ TransactionReportsStorage/TransactionReportsStorage.UIDevExpress/Form1.cs
@@ -30,11 +30,31 @@ namespace TransactionReportsStorage.UIDevExpress
             labelControl1.Text = $"{this._records.PageNumber} of {this._records.TotalPages}";
         }
 
+        private async Task SetFilteredPage(int pageNumber)
+        {
+            var pageParameters = new PageParameters();
+            pageParameters.PageNumber = pageNumber;
+            this._records = await this._recordsService.GetPageAsync(pageParameters, 
+                this.textEdit1.Text, this.dateEdit1.DateTime, this.dateEdit2.DateTime);
+            this.gridControl1.DataSource = this._records;
+            this.gridControl1.Refresh();
+            labelControl1.Text = $"{this._records.PageNumber} of {this._records.TotalPages}";
+        }
+
         private async void simpleButton1_Click(object sender, EventArgs e)
         {
             if (this._records.HasPreviousPage)
             {
-                await this.SetPage(this._records.PageNumber - 1);
+                if (!string.IsNullOrEmpty(this.textEdit1.Text)
+                    && !string.IsNullOrEmpty(this.dateEdit1.Text)
+                    && !string.IsNullOrEmpty(this.dateEdit2.Text))
+                {
+                    await this.SetFilteredPage(this._records.PageNumber - 1);
+                }
+                else
+                {
+                    await this.SetPage(this._records.PageNumber - 1);
+                }
             }
         }
 
@@ -42,7 +62,30 @@ namespace TransactionReportsStorage.UIDevExpress
         {
             if (this._records.HasNextPage)
             {
-                await this.SetPage(this._records.PageNumber + 1);
+                if (!string.IsNullOrEmpty(this.textEdit1.Text)
+                    && !string.IsNullOrEmpty(this.dateEdit1.Text)
+                    && !string.IsNullOrEmpty(this.dateEdit2.Text))
+                {
+                    await this.SetFilteredPage(this._records.PageNumber + 1);
+                }
+                else
+                {
+                    await this.SetPage(this._records.PageNumber + 1);
+                }
+            }
+        }
+
+        private async void simpleButton3_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(this.textEdit1.Text) 
+                && !string.IsNullOrEmpty(this.dateEdit1.Text) 
+                && !string.IsNullOrEmpty(this.dateEdit2.Text))
+            {
+                await this.SetFilteredPage(1);
+            }
+            else
+            {
+                await this.SetPage(1);
             }
         }
     }
