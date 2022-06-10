a24b9abce62922e91706f1710db0da48da932207
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-06-10 10:35:21 +0300

    Created DTOs.

---
+++ TransactionReportsStorage/TransactionReportsStorage.Application/Models/DTO/RecordDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TransactionReportsStorage.Application.Models.DTO
+{
+    public class RecordDto : EntityBaseDto
+    {
+        public string Name { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public UserDto User { get; set; }
+
+        public CellDto Cell { get; set; }
+    }
+}
---
+++ TransactionReportsStorage/TransactionReportsStorage.Application/Models/DTO/UserDto.cs
@@ -0,0 +1,7 @@
+﻿namespace TransactionReportsStorage.Application.Models.DTO
+{
+    public class UserDto : EntityBaseDto
+    {
+        public string Name { get; set; }
+    }
+}
