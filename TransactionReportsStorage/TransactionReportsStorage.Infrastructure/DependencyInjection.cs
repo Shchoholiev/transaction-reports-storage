37f9786772fe8ce1d1ed05022b7099c56e0bb67d
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-06-10 12:07:41 +0300

    Configured DI.

---
+++ TransactionReportsStorage/TransactionReportsStorage.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TransactionReportsStorage.Application.Interfaces;
+using TransactionReportsStorage.Application.Interfaces.Repositories;
+using TransactionReportsStorage.Infrastructure.EF;
+using TransactionReportsStorage.Infrastructure.Repositories;
+using TransactionReportsStorage.Infrastructure.Services;
+
+namespace TransactionReportsStorage.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, 
+                                                           IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("SQLDataBase");
+
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(connectionString)
+            );
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRecordsService, RecordsService>();
+
+            return services;
+        }
+    }
+}
