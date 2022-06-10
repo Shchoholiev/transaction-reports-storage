edbc5bb113c837a4717c8a72bd59e74de961c4be
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-06-10 10:59:58 +0300

    Added EF Context.

---
+++ TransactionReportsStorage/TransactionReportsStorage.Infrastructure/EF/ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TransactionReportsStorage.Core.Entities;
+
+namespace TransactionReportsStorage.Infrastructure.EF
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext()
+        {
+        }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        public DbSet<Record> Records { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Cell> Cells { get; set; }
+    }
+}
