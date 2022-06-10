46cb2645408e7e4569fccce13a9154a23cc24434
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-06-10 10:50:06 +0300

    Added inteface for Records Service.

---
+++ TransactionReportsStorage/TransactionReportsStorage.Application/Interfaces/IRecordsService.cs
@@ -0,0 +1,11 @@
+﻿using TransactionReportsStorage.Application.Paging;
+using TransactionReportsStorage.Core.Entities;
+
+namespace TransactionReportsStorage.Application.Interfaces
+{
+    public interface IRecordsService
+    {
+        Task<PagedList<Record>> GetPageAsync(PageParameters pageParameters, string? cellName, 
+                                             DateOnly? startDate, DateOnly? endDate);
+    }
+}
