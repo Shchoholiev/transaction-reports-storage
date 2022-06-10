97c1907e379ff0543a0c2bd416944cef7364effb
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-06-10 10:43:11 +0300

    Added classes for paging.

---
+++ TransactionReportsStorage/TransactionReportsStorage.Application/Paging/PageParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace TransactionReportsStorage.Application.Paging
+{
+    public class PageParameters
+    {
+        public int PageSize { get; set; } = 12;
+
+        public int PageNumber { get; set; } = 1;
+    }
+}
